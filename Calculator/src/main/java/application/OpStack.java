3c1c1657c22748f3514c8f60d3b4e54bcbe92a71
Mahid <mahid.mirza234@gmail.com>
2023-11-21 14:31:21 +0000

    Added pop() method to OpStack with its tests

--- Calculator/src/main/java/application/OpStack.java
+++ Calculator/src/main/java/application/OpStack.java
@@ -1,20 +1,27 @@
 package application;
 
+import java.util.EmptyStackException;
 import java.util.Stack;
 
 public class OpStack {
-    private Stack<Entry> opStack;
+  private Stack<Entry> opStack;
 
-    public OpStack() {
-        opStack = new Stack<>();
-    }
-
-    public int size() {
-        return opStack.size();
-    }
-
-    public void push(Entry entry) {
-      opStack.push(entry);
+  public OpStack() {
+    opStack = new Stack<>();
   }
 
+  public int size() {
+    return opStack.size();
+  }
+
+  public void push(Entry entry) {
+    opStack.push(entry);
+  }
+
+  public Entry pop() {
+    if (opStack.isEmpty()) {
+      throw new EmptyStackException();
+    }
+    return opStack.pop();
+  }
 }
--- Calculator/src/test/java/application/OpStackTest.java
+++ Calculator/src/test/java/application/OpStackTest.java
@@ -3,27 +3,48 @@ package application;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import java.util.EmptyStackException;
 import org.junit.jupiter.api.Assertions;
 
 public class OpStackTest {
 
-    private OpStack stack;
+  private OpStack stack;
 
-    @BeforeEach
-    public void setUp() {
-        stack = new OpStack();
-    }
+  @BeforeEach
+  public void setUp() {
+    stack = new OpStack();
+  }
 
-    @Test
-    public void newStackShouldBeEmpty() {
-        Assertions.assertEquals(0, stack.size(), "A new stack should have size 0");
-    }
-    
-    @Test
-    public void sizeShouldIncreaseAfterPush() {
-        stack.push(new Entry(Symbol.PLUS)); // Use the PLUS symbol as an example
-        Assertions.assertEquals(1, stack.size(), "The stack should have size 1 after pushing one item");
-    }
+  @Test
+  public void newStackShouldBeEmpty() {
+    Assertions.assertEquals(0, stack.size(), "A new stack should have size 0");
+  }
+
+  @Test
+  public void sizeShouldIncreaseAfterPush() {
+    stack.push(new Entry(Symbol.PLUS)); // Use the PLUS symbol as an example
+    Assertions.assertEquals(1, stack.size(), "The stack should have size 1 after pushing one item");
+  }
+
+  @Test
+  public void shouldPopCorrectEntryAndDecreaseSize() {
+    Entry entry = new Entry(Symbol.PLUS);
+    stack.push(entry);
+    Assertions.assertEquals(1, stack.size(), "The stack should have size 1 after pushing one item");
+
+    Entry poppedEntry = stack.pop();
+    Assertions.assertEquals(entry, poppedEntry,
+        "The popped entry should be the same as the pushed one");
+    Assertions.assertEquals(0, stack.size(), "The stack should be empty after popping");
+  }
+
+  @Test
+  public void popOnEmptyStackShouldThrowException() {
+    Assertions.assertThrows(EmptyStackException.class, () -> {
+      stack.pop();
+    }, "Popping from an empty stack should throw EmptyStackException");
+  }
 
 }
+
 
