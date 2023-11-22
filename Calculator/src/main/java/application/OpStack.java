953bb03366c6932068505375ba459d40ab56a5a3
Mahid <mahid.mirza234@gmail.com>
2023-11-22 10:48:42 +0000

    Added Javadoc and useful comments to my OpStack and OpStackTest files

--- Calculator/src/main/java/application/OpStack.java
+++ Calculator/src/main/java/application/OpStack.java
@@ -3,31 +3,60 @@ package application;
 import java.util.EmptyStackException;
 import java.util.Stack;
 
+/**
+ * The OpStack class represents a stack data structure specifically for handling Entry objects. It
+ * offers basic stack operations such as push, pop, and peek (top).
+ */
 public class OpStack {
-  private Stack<Entry> opStack;
+  private Stack<Entry> opStack; // Stack to store Entry objects
 
+  /**
+   * Constructor for OpStack. Initialises a new Stack.
+   */
   public OpStack() {
     opStack = new Stack<>();
   }
 
+  /**
+   * Returns the number of entries in the stack.
+   * 
+   * @return The size of the stack.
+   */
   public int size() {
     return opStack.size();
   }
 
+  /**
+   * Pushes an entry onto the top of the stack.
+   * 
+   * @param entry The Entry object to be pushed onto the stack.
+   */
   public void push(Entry entry) {
     opStack.push(entry);
   }
 
+  /**
+   * Removes and returns the top entry from the stack.
+   * 
+   * @return The Entry object at the top of the stack.
+   * @throws EmptyStackException if the stack is empty.
+   */
   public Entry pop() {
     if (opStack.isEmpty()) {
-      throw new EmptyStackException();
+      throw new EmptyStackException(); // Throw exception if trying to pop from an empty stack
     }
     return opStack.pop();
   }
 
+  /**
+   * Retrieves, but does not remove, the top entry of the stack.
+   * 
+   * @return The Entry object at the top of the stack.
+   * @throws EmptyStackException if the stack is empty.
+   */
   public Entry top() {
     if (opStack.isEmpty()) {
-      throw new EmptyStackException();
+      throw new EmptyStackException(); // Throw exception if trying to peek at an empty stack
     }
     return opStack.peek();
   }
--- Calculator/src/test/java/application/OpStackTest.java
+++ Calculator/src/test/java/application/OpStackTest.java
@@ -1,6 +1,5 @@
 package application;
 
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.util.EmptyStackException;
@@ -15,17 +14,24 @@ public class OpStackTest {
     stack = new OpStack();
   }
 
+  // Test 1: Verify that a new stack is empty
+  // This was the first test written. To make it pass, the size() method was implemented in OpStack.
   @Test
   public void newStackShouldBeEmpty() {
     Assertions.assertEquals(0, stack.size(), "A new stack should have size 0");
   }
 
+  // Test 2: Ensure that the size increases after pushing an entry
+  // This test led to the implementation of the push() method in OpStack.
   @Test
   public void sizeShouldIncreaseAfterPush() {
     stack.push(new Entry(Symbol.PLUS));
     Assertions.assertEquals(1, stack.size(), "The stack should have size 1 after pushing one item");
   }
 
+  // Test 3: Check if popping an entry decreases the size and returns the correct entry
+  // To pass this test, the pop() method was implemented with logic to handle empty stacks and
+  // return the correct entry.
   @Test
   public void shouldPopCorrectEntryAndDecreaseSize() {
     Entry entry = new Entry(Symbol.PLUS);
@@ -38,6 +44,8 @@ public class OpStackTest {
     Assertions.assertEquals(0, stack.size(), "The stack should be empty after popping");
   }
 
+  // Test 4: Verify that popping from an empty stack throws an exception
+  // This was added to test the exception handling of the pop() method.
   @Test
   public void popOnEmptyStackShouldThrowException() {
     Assertions.assertThrows(EmptyStackException.class, () -> {
@@ -45,6 +53,8 @@ public class OpStackTest {
     }, "Popping from an empty stack should throw EmptyStackException");
   }
 
+  // Test 5: Ensure the top() method returns the correct entry without removing it
+  // This led to the implementation of the top() method in OpStack.
   @Test
   public void topShouldReturnCorrectEntryWithoutRemoving() {
     Entry entry = new Entry(Symbol.PLUS);
@@ -57,6 +67,8 @@ public class OpStackTest {
         "The stack size should remain the same after calling top()");
   }
 
+  // Test 6: Check if calling top() on an empty stack throws an exception
+  // This was added to ensure proper exception handling in the top() method.
   @Test
   public void topOnEmptyStackShouldThrowException() {
     Assertions.assertThrows(EmptyStackException.class, () -> {
@@ -64,5 +76,3 @@ public class OpStackTest {
     }, "Calling top on an empty stack should throw EmptyStackException");
   }
 }
-
-
