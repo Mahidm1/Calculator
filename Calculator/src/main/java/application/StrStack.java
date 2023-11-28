a3d5b7eeeeeea6a6e2b2951609037009d2f90266
Mahid <mahid.mirza234@gmail.com>
2023-11-28 14:52:28 +0000

    Added Javadoc and useful comments to my StrStack and its test

--- Calculator/src/main/java/application/StrStack.java
+++ Calculator/src/main/java/application/StrStack.java
@@ -2,21 +2,45 @@ package application;
 
 import java.util.EmptyStackException;
 
+/**
+ * StrStack is a stack data structure specifically for storing strings. It provides basic stack
+ * operations like push, pop, and top, along with a method to check the current size of the stack.
+ */
 public class StrStack {
   private Stack stack;
 
+  /**
+   * Constructs a new StrStack instance. Initialises an empty stack to hold string entries.
+   */
   public StrStack() {
     this.stack = new Stack();
   }
 
+  /**
+   * Returns the current size of the stack.
+   *
+   * @return the number of items currently in the stack.
+   */
   public int size() {
     return this.stack.size();
   }
 
+  /**
+   * Pushes a new string item onto the top of the stack.
+   *
+   * @param item the string item to be pushed onto the stack.
+   */
   public void push(String item) {
     this.stack.push(new Entry(item));
   }
 
+  /**
+   * Removes and returns the top item from the stack. If the stack is empty, it throws an
+   * EmptyStackException.
+   *
+   * @return the string item that was at the top of the stack.
+   * @throws EmptyStackException if the stack is empty when the method is called.
+   */
   public String pop() {
     if (this.stack.size() == 0) {
       throw new EmptyStackException();
@@ -24,11 +48,17 @@ public class StrStack {
     return this.stack.pop().getString();
   }
 
+  /**
+   * Returns the item at the top of the stack without removing it. If the stack is empty, it throws
+   * an EmptyStackException.
+   *
+   * @return the string item currently at the top of the stack.
+   * @throws EmptyStackException if the stack is empty when the method is called.
+   */
   public String top() {
     if (this.stack.size() == 0) {
       throw new EmptyStackException();
     }
     return this.stack.top().getString();
   }
-
 }
--- Calculator/src/test/java/application/StrStackTest.java
+++ Calculator/src/test/java/application/StrStackTest.java
@@ -2,17 +2,21 @@ package application;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import application.StrStack;
 import java.util.EmptyStackException;
 
 public class StrStackTest {
 
+  // Test 1: Check if a new stack has size 0. This is the simplest test and likely the first one
+  // written.
+  // To make this test pass, the StrStack constructor must initialise the size to 0.
   @Test
   public void testStackSizeWhenEmpty() {
     StrStack stack = new StrStack();
     Assertions.assertEquals(0, stack.size(), "Stack size should be 0 when empty");
   }
 
+  // Test 2: After adding one item to the stack, the size should be 1.
+  // To pass this test, the push method must increment the stack's size.
   @Test
   public void testStackSizeAfterPush() {
     StrStack stack = new StrStack();
@@ -20,6 +24,8 @@ public class StrStackTest {
     Assertions.assertEquals(1, stack.size(), "Stack size should be 1 after pushing one item");
   }
 
+  // Test 3: When an item is popped, it should be returned and the stack size should decrease.
+  // To pass this test, the pop method must both return the last item and decrement the size.
   @Test
   public void testPop() {
     StrStack stack = new StrStack();
@@ -30,6 +36,9 @@ public class StrStackTest {
     Assertions.assertEquals(0, stack.size(), "Stack size should be 0 after one pop");
   }
 
+  // Test 4: Popping from an empty stack should throw an EmptyStackException.
+  // This test ensures the stack correctly handles underflow errors.
+  // To pass, the pop method must throw an exception when the stack is empty.
   @Test
   public void testPopEmptyStackThrowsException() {
     StrStack stack = new StrStack();
@@ -38,6 +47,8 @@ public class StrStackTest {
     }, "Popping from an empty stack should throw EmptyStackException");
   }
 
+  // Test 5: Checking the top item should return the last item pushed without removing it.
+  // To pass this test, the top method must retrieve the last item without altering the size.
   @Test
   public void testTop() {
     StrStack stack = new StrStack();
@@ -48,6 +59,9 @@ public class StrStackTest {
     Assertions.assertEquals(2, stack.size(), "The stack size should remain 2 after calling top");
   }
 
+  // Test 6: Similar to test 4 but for the top method. The top method should throw an exception if
+  // the stack is empty.
+  // This test is to ensure that the top method doesn't allow peeking at an empty stack.
   @Test
   public void testTopEmptyStackThrowsException() {
     StrStack stack = new StrStack();
