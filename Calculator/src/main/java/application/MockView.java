4ae7330330bc2e1e602ee6989344913f1a84fcf9
Mahid <mahid.mirza234@gmail.com>
2023-12-10 15:36:11 +0000

    Added javadocs and comments to my files from my previous commit

--- Calculator/src/main/java/application/MockView.java
+++ Calculator/src/main/java/application/MockView.java
@@ -2,37 +2,76 @@ package application;
 
 import java.util.function.Consumer;
 
+/**
+ * A mock implementation of the ViewInterface for testing purposes. This class simulates the
+ * behaviour of a user interface without the need for an actual GUI.
+ */
 public class MockView implements ViewInterface {
+  // Fields to store the expression and the answer.
   public String expression;
   public String answer;
+
+  // A consumer to handle type change events.
   public Consumer<OpType> typeChange;
 
+  /**
+   * Adds a calculation observer. This method would typically be used to set up a listener for
+   * calculation requests. In this mock implementation, it's left empty as actual calculation
+   * triggering is not needed.
+   *
+   * @param f The Runnable to be executed for calculation.
+   */
   @Override
   public void addCalculateObserver(Runnable f) {}
 
+  /**
+   * Adds a type observer to handle changes in calculation types (e.g., infix, reverse polish).
+   * Stores the provided Consumer to be used in setType method.
+   *
+   * @param c The Consumer accepting OpType to handle type changes.
+   */
   @Override
   public void addTypeObserver(Consumer<OpType> c) {
     this.typeChange = c;
   }
 
+  /**
+   * Returns the current expression. This method simulates getting an expression from a user input
+   * field in a GUI.
+   *
+   * @return The current expression.
+   */
   @Override
   public String getExpression() {
     return this.expression;
   }
 
+  /**
+   * Sets the answer. This method simulates displaying the answer in a GUI.
+   *
+   * @param a The answer to be displayed.
+   */
   @Override
   public void setAnswer(String a) {
     this.answer = a;
   }
 
+  /**
+   * Sets the calculation type and triggers the type change event. This method simulates a user
+   * changing the type of calculation in a GUI.
+   *
+   * @param type The OpType to set and trigger.
+   */
   public void setType(OpType type) {
     if (this.typeChange != null) {
       this.typeChange.accept(type);
     }
   }
 
+  /**
+   * Starts the view. This method would typically initialise and display the GUI. In this mock
+   * implementation, it's left empty.
+   */
   @Override
-  public void startView() {
-
-  }
+  public void startView() {}
 }
--- Calculator/src/test/java/application/CalcControllerTest.java
+++ Calculator/src/test/java/application/CalcControllerTest.java
@@ -17,19 +17,25 @@ public class CalcControllerTest {
     controller = new CalcController(calcModel, mockView);
   }
 
+  // Test 1: Test if CalcController correctly handles calculation for infix expressions.
+  // This was the first test written in TDD. To make this test pass, handleCalculation method was implemented
+  // in CalcController to use CalcModel's evaluate method for infix expressions.
   @Test
   public void testHandleCalculationInfix() {
-    mockView.expression = "3 + 2";
-    mockView.setType(OpType.STANDARD);
-    controller.handleCalculation();
+    mockView.expression = "3 + 2"; 
+    mockView.setType(OpType.STANDARD); 
+    controller.handleCalculation(); 
     assertEquals("5.00", mockView.answer, "Calculation for infix expression is incorrect");
   }
 
+  // Test 2: Test if CalcController correctly handles calculation for RPN expressions.
+  // This was the second test written. To pass this test, modifications were made in handleCalculation
+  // to support RPN evaluations, including setting the isUsingInfix flag based on the type.
   @Test
   public void testHandleCalculationRPN() {
-    mockView.expression = "3 2 +";
-    mockView.setType(OpType.REV_POLISH);
-    controller.handleCalculation();
+    mockView.expression = "3 2 +"; 
+    mockView.setType(OpType.REV_POLISH); 
+    controller.handleCalculation(); 
     assertEquals("5.00", mockView.answer, "Calculation for RPN expression is incorrect");
   }
 }
